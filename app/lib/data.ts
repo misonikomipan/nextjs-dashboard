cb80abee60ac7b1e53eaf4ba49004a84cb18984b
misonikomipan <yusakum1311@gmail.com>
2024-04-26 01:31:59 +0900

    [Delete] experiment data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,11 +16,6 @@ export async function fetchRevenue() {
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
